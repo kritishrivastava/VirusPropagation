36c6c5bc71fa2e8e98de999ec32d7e3f36970daf
Kriti Shrivastava <kriti3009@gmail.com>
2017-07-13 19:11:33 -0400

    main script

--- virusPropagation.py
+++ virusPropagation.py
@@ -41,25 +41,25 @@ def calculateEffectiveStrength(b , d, lambda1):
     print("Minimum transmission probability for epidemic: ",np.real(min_beta))
     #Find maximum healing probability (δ) that results in a networkwide epidemic i.e. strength > 1
     max_delta = b * lambda1
-    print("Maximum heaing probability for epidemic: ",np.real(max_delta))
+    print("Maximum healing probability for epidemic: ",np.real(max_delta))
 
 
-def plotSvsB(b, d, lambda1):
+def plotSvsB(b, d, lambda1, part):
     #Input: transmission probability β, healing probability δ and highest eigenvalue for adjacency matrix lambda1
     #Output: Plots a graph for Relationship between Virus Strength and Transmission probability (beta)
-    betas = np.arange(0, b+0.01, 0.01)
+    betas = np.arange(0, b+0.001, 0.001)
     strengths = [lambda1*(beta/d) for beta in betas]
     plt.figure(figsize=(12, 6))
     plt.plot(betas, strengths, "-o")
     plt.title("Relationship between Virus Strength and Transmission probability (beta)")
     plt.xlabel('Beta')
     plt.ylabel('Effective Strength')
-    plotFile = "output/" + "staticGraph_betaVSstrength.png"
+    plotFile = "output/" + "betaVSstrength_"+ part +".png"
     os.makedirs(os.path.dirname(plotFile), exist_ok=True)
     plt.savefig(plotFile, bbox_inches='tight')
 
 
-def plotSvsD(b, d, lambda1):
+def plotSvsD(b, d, lambda1, part):
     # Input: transmission probability β, healing probability δ and highest eigenvalue for adjacency matrix lambda1
     # Output: Plots a graph for Relationship between Virus Strength and Healing probability (delta)
     deltas = np.arange(0.01, d+0.1, 0.1)
@@ -69,7 +69,7 @@ def plotSvsD(b, d, lambda1):
     plt.title("Relationship between Virus Strength and Healing probability (delta)")
     plt.xlabel('Delta')
     plt.ylabel('Effective Strength')
-    plotFile = "output/" + "staticGraph_deltaVSstrength.png"
+    plotFile = "output/" + "deltaVSstrength_"+ part +".png"
     os.makedirs(os.path.dirname(plotFile), exist_ok=True)
     plt.savefig(plotFile, bbox_inches='tight')
 
@@ -91,9 +91,9 @@ if __name__ == "__main__":
     print("-------------  Results for beta1(0.20) and delta1(0.70)  -------------")
     calculateEffectiveStrength(b, d, lambda1)
     #Plot graph for strength vs b (d fixed)
-    plotSvsB(b, d, lambda1)
+    plotSvsB(b, d, lambda1, "part1")
     #Plot graph for strength vs d (b fixed)
-    plotSvsD(b, d, lambda1)
+    plotSvsD(b, d, lambda1, "part1")
 
     ### Part 2 for b2 and d2
     # Setting the value for Transmission probability(b) and healing probability(d)
@@ -101,3 +101,7 @@ if __name__ == "__main__":
     d = 0.60
     print("\n\n-------------  Results for beta1(0.01) and delta1(0.60)  -------------")
     calculateEffectiveStrength(b, d, lambda1)
+    # Plot graph for strength vs b (d fixed)
+    plotSvsB(b, d, lambda1, "part2")
+    # Plot graph for strength vs d (b fixed)
+    plotSvsD(b, d, lambda1, "part2")
